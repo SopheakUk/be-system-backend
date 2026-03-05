de76efdf030d705991c7661e484c2d7a971f4a3d
Sopheak Uk <uk.sopheak@gmail.com>
2026-03-05 08:33:17 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+    swagger(app);
+
     await app.listen(process.env.PORT ?? 3001);
 }
+
+function swagger(app: INestApplication<any>) {
+    const config = new DocumentBuilder()
+        .setTitle('Nest API')
+        .setDescription('API documentation')
+        .setVersion('1.0')
+        .addBearerAuth() // optional, if using JWT
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+}
+
 bootstrap();
